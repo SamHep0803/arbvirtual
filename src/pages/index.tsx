0aa3905d4571ae4b846793f4e0abd9887a924dd3
Samuel Hepworth <samhep0803@gmail.com>
2022-07-17 21:32:22 +0400

    fix: use proper colours and add fix for vercel and the backdrop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,45 +4,47 @@ import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex bg-opacity-60 bg-black text-zinc-200 min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Arabian Virtual Airline</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="flex min-h-screen bg-main-img bg-no-repeat bg-cover bg-center">
+      <div className="flex backdrop-blur-[2px] bg-opacity-60 bg-black text-zinc-200 font-sans min-h-screen min-w-full flex-col items-center justify-center py-2">
+        <Head>
+          <title>Arabian Virtual Airline</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <Image
-          src="/logo.png"
-          alt="Arabian Virtual Airline"
-          width={400}
-          height={200}
-        />
-        <h1 className="text-6xl font-bold">
-          Welcome to{" "}
-          <a className="text-yellow-500" href="https://nextjs.org">
-            Arabian Virtual.
-          </a>
-        </h1>
+        <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+          <Image
+            src="/logo.png"
+            alt="Arabian Virtual Airline"
+            width={200}
+            height={200}
+          />
+          <h1 className="text-6xl font-bold">
+            Welcome to{" "}
+            <span className="text-[#ffcb52]">
+              Arabian Virtual.
+            </span>
+          </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by registering below. &darr;
-        </p>
+          <p className="mt-3 text-2xl">
+            Get started by registering below. &darr;
+          </p>
 
-        <div className="p-8 flex max-w-xs flex-wrap items-center justify-between sm:w-full">
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-2  rounded-xl bg-yellow-600 px-4 py-3 text-left hover:bg-yellow-700 focus:bg-yellow-700 transition"
-          >
-            <h3 className="text-2xl font-bold">Register</h3>
-          </a>
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-2 rounded-xl bg-yellow-600 px-4 py-3 text-left hover:bg-yellow-700 focus:bg-yellow-700 transition"
-          >
-            <h3 className="text-2xl font-bold">Login</h3>
-          </a>
-        </div>
-      </main>
+          <div className="p-8 flex max-w-xs flex-wrap items-center justify-between sm:w-full">
+            <a
+              href="https://vamsys.io/register/arabian"
+              className="mt-2 text-white rounded-xl border-2 border-[#ffcb52]  px-4 py-3 text-left hover:bg-yellow-600 focus:bg-yellow-600 transition"
+            >
+              <h3 className="text-2xl font-normal">Register</h3>
+            </a>
+            <a
+              href="https://vamsys.io/login/arabian"
+              className="mt-2 rounded-xl border-2 border-[#ffcb52] px-4 py-3 text-left hover:bg-yellow-600 focus:bg-yellow-600 transition"
+            >
+              <h3 className="text-2xl font-normal">Login</h3>
+            </a>
+          </div>
+        </main>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
 	content: ["./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				"main-img": "url('/bg.png')",
+			},
+			fontFamily: {
+				sans: ["Proxima Nova", ...defaultTheme.fontFamily.sans],
+			},
+		},
 	},
 	plugins: [],
 };
